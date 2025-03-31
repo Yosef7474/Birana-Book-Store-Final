27b68f877f809cac992652fbb0e40b9d1b1e84d0
Yosef7474 <yosefdejene747@gmail.com>
2025-03-31 15:09:44 +0300

    change to new url

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,5 +1,6 @@
 const getBaseUrl = () => {
-    return "http://localhost:5000"
+    // return "http://localhost:5000"
+    return 'https://birana-book-store-final.onrender.com'
 }
 
 export default getBaseUrl;
